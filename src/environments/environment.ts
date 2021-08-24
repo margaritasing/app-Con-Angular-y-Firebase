df5709b3bf7d6e0456a47deb7343f8a2f7295b72
Libia M Freites M <61855440+margaritasing@users.noreply.github.com>
2021-08-24 13:32:25 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBW34NkJN_piF4SH_90GmW1e4483XopgIc",
-    authDomain: "empleados-d5164.firebaseapp.com",
-    projectId: "empleados-d5164",
-    storageBucket: "empleados-d5164.appspot.com",
-    messagingSenderId: "378543379624",
-    appId: "1:378543379624:web:640a58e74653b4d5f79e9d"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   }
 };
 
